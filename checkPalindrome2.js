46bdeed5d78643c953a7a9c7f12fa12c0fc979d0
SAGUN JAISWAL <sagunjaiswal99@gmail.com>
2021-10-29 21:46:24 +0530

    Update checkPalindrome2.js

--- checkPalindrome2.js
+++ checkPalindrome2.js
@@ -11,3 +11,11 @@ if(isPalindrome(str)){
 else{
   document.write("It is not a palindrome");
 }
+/*
+The split('') method converts the string into individual array characters.
+const arrayValues = string.split(''); // ["h", "e", "l", "l", "o"]
+The reverse() method reverses the position in an array.
+// ["o", "l", "l", "e", "h"]
+const reverseArrayValues = arrayValues.reverse();
+The join('') method joins all the elements of an array into a string.
+const reverseString = reverseArrayValues.join(''); // "olleh"*/
