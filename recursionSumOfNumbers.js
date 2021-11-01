cd505532ea9a7a85f5dccb7d9fed406731e24383
SAGUN JAISWAL <sagunjaiswal99@gmail.com>
2021-11-01 18:37:43 +0530

    Create recursionSumOfNumbers.js

---
+++ recursionSumOfNumbers.js
@@ -0,0 +1,11 @@
+function sum(num) {
+    if(num > 0) {
+        return num + sum(num - 1);
+    }
+    else {
+        return num;
+    }
+ }
+const number = parseInt(prompt('Enter a positive integer: '));
+const result = sum(number);
+console.log(`The sum is ${result}`);
