1f58ac4ea77a90584f961ac5d3d89cc52aed99df
SAGUN JAISWAL <sagunjaiswal99@gmail.com>
2021-11-01 16:11:24 +0530

    Create checkPrimeNumber.js

---
+++ checkPrimeNumber.js
@@ -0,0 +1,19 @@
+const num = parseInt(prompt("Enter a number : "));
+let isPrime = true;
+if(num == 1){
+	console.log("1 is neither prime nor composite number.");
+}
+else if(num > 1){
+	for(let i = 2 ; i < num ; i++){
+  	if(num % i == 0){
+    	isPrime = false;
+      break;
+    }
+  }
+}
+if(isPrime){
+	console.log(`${num} is a prime number`);
+}
+else{
+	console.log(`${num} is not a prime number`);
+}
