3462867f39ea1d5021c9d6ce70e17dd844f3c9fc
SAGUN JAISWAL <sagunjaiswal99@gmail.com>
2021-11-01 19:11:28 +0530

    Create triangleArea.js

---
+++ triangleArea.js
@@ -0,0 +1,6 @@
+const side1 = parseInt(prompt('Enter side1: '));
+const side2 = parseInt(prompt('Enter side2: '));
+const side3 = parseInt(prompt('Enter side3: '));
+const s = (side1 + side2 + side3) / 2;
+const areaValue = Math.sqrt(s * (s - side1) * (s - side2) * (s - side3));
+console.log(`The area of the triangle is ${areaValue}`);
